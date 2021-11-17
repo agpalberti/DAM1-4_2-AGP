df0ae68aeb20207e15189269bc45d882f2d25429
agpalberti <agonzalezp@iesrafaelalberti.es>
2021-11-17 21:43:35 +0100

    varios metodos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,18 @@
-
 package un4.collections
 
-data class Tienda(val nombre: String, val clientes: List<Clientes>)
+data class Tienda(val nombre: String, val clientes: List<Clientes>) {
+
+    fun obtenerConjuntoDeClientes(): Set<Clientes> = clientes.toSet() //Preguntar a edu sobre output
+
+    fun obtenerCiudadesDeClientes(): Set<Ciudad> {
+        val ciudades = mutableSetOf<Ciudad>()
+        clientes.forEach { ciudades.add(it.ciudad) }
+        return ciudades.toSet()
+    }
+
+    fun obtenerClientesPorCiudad(ciudad:Ciudad): List<Clientes>{}
+
+}
 
 data class Clientes(val nombre: String, val ciudad: Ciudad, val pedidos: List<Pedido>) {
     override fun toString() = "$nombre from ${ciudad.nombre}"
@@ -16,3 +27,4 @@ data class Producto(val nombre: String, val precio: Double) {
 data class Ciudad(val nombre: String) {
     override fun toString() = nombre
 }
+
