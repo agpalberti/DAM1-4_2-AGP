e10a0d4c94a5a6c1507941035d75e888e6f4ae51
agpalberti <agonzalezp@iesrafaelalberti.es>
2021-11-18 01:47:05 +0100

    Terminado

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,7 @@ package un4.collections
 
 data class Tienda(val nombre: String, val clientes: List<Clientes>) {
 
-    fun obtenerConjuntoDeClientes(): Set<Clientes> = clientes.toSet() //Preguntar a edu sobre output
+    fun obtenerConjuntoDeClientes(): Set<Clientes> = clientes.toSet()
 
     fun obtenerCiudadesDeClientes(): Set<Ciudad> {
         val ciudades = mutableSetOf<Ciudad>()
@@ -10,8 +10,32 @@ data class Tienda(val nombre: String, val clientes: List<Clientes>) {
         return ciudades.toSet()
     }
 
-    fun obtenerClientesPorCiudad(ciudad:Ciudad): List<Clientes>{}
+    fun obtenerClientesPorCiudad(ciudad: Ciudad): List<Clientes> = clientes.filter { it.ciudad == ciudad }
 
+    fun checkTodosClientesSonDe(ciudad: Ciudad): Boolean = clientes.all { it.ciudad == ciudad }
+
+    fun hayClientesDe(ciudad: Ciudad): Boolean = clientes.any { it.ciudad == ciudad }
+
+    fun cuentaClientesDe(ciudad: Ciudad): Int = clientes.count { it.ciudad == ciudad }
+
+    fun encuentraClienteDe(ciudad: Ciudad): Clientes? = clientes.find { it.ciudad == ciudad }
+
+    fun obtenerClientesOrdenadosPorPedidos(): List<Clientes> = clientes.sortedBy { it.pedidos.size }
+
+    fun obtenerClientesConPedidosSinEntregar(): Set<Clientes> =
+        clientes.filter { it -> it.pedidos.any { !it.estaEntregado } }.toSet()
+
+    fun obtenerProductosPedidosPorCliente(cliente: Clientes): List<Producto> {
+        val listaProductos = mutableListOf<Producto>()
+        cliente.pedidos.forEach { it -> it.productos.forEach { listaProductos.add(it) } }
+        return listaProductos.toList()
+    }
+
+    fun obtenerProductosPedidos(): Set<Producto> {
+        val setProducto = mutableSetOf<Producto>()
+        clientes.forEach { it -> obtenerProductosPedidosPorCliente(it).forEach{setProducto.add(it)} }
+        return setProducto.toSet()
+    }
 }
 
 data class Clientes(val nombre: String, val ciudad: Ciudad, val pedidos: List<Pedido>) {
