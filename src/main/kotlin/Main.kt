c69e753e111f12feb12dbb8a11b8d095547b076d
agpalberti <agonzalezp@iesrafaelalberti.es>
2021-11-19 19:53:30 +0100

    fgjctud

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -30,16 +30,35 @@ data class Tienda(val nombre: String, val clientes: List<Clientes>) {
         clientes.forEach { it -> it.obtenerProductosPedidos().forEach{setProducto.add(it)} }
         return setProducto.toSet()
     }
+
+    fun obtenerProductosPedidosPorTodos():Set<Producto>{
+        val setProducto = mutableSetOf<Producto>()
+        clientes.forEach{  }//TODO
+    }
+
+    fun obtenerNumeroVecesProductoPedido(producto:Producto):Int{} //TODO
+
+    fun agrupaClientesPorCiudad():Map<Ciudad,List<Clientes>>{} //TODO
+
+    fun mapeaNombreACliente(): Map<String,Clientes>{} //TODO
+
+    fun mapeaClienteACiudad(): Map<Clientes,Ciudad>{} //TODO
+
+    fun mapeaNombreClienteACiudad
+
+
 }
 
 data class Clientes(val nombre: String, val ciudad: Ciudad, val pedidos: List<Pedido>) {
     override fun toString() = "$nombre from ${ciudad.nombre}"
 
-    fun obtenerProductosPedidos(): Set<Producto> {
-        val setProductos = mutableSetOf<Producto>()
-        pedidos.forEach { it -> it.productos.forEach { setProductos.add(it) } }
-        return setProductos.toSet()
+    fun obtenerProductosPedidos(): List<Producto> {
+        val listaProductos = mutableListOf<Producto>()
+        pedidos.forEach { it -> it.productos.forEach { listaProductos.add(it) } }
+        return listaProductos.toList()
     }
+
+    fun encuentraProductoMasCaro():Producto? = pedidos.filter{ it.estaEntregado } //TODO
 }
 
 data class Pedido(val productos: List<Producto>, val estaEntregado: Boolean)
