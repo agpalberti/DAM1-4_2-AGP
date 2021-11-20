b9bc4c9c3ac87e579668193b80d761daff11c120
agpalberti <agonzalezp@iesrafaelalberti.es>
2021-11-20 04:37:46 +0100

    Terminados muchos metodos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,5 @@
 package un4.collections
 
-import kotlin.math.max
-
 data class Tienda(val nombre: String, val clientes: List<Clientes>) {
 
     fun obtenerConjuntoDeClientes(): Set<Clientes> = clientes.toSet()
@@ -27,29 +25,50 @@ data class Tienda(val nombre: String, val clientes: List<Clientes>) {
     fun obtenerClientesConPedidosSinEntregar(): Set<Clientes> =
         clientes.filter { it -> it.pedidos.any { !it.estaEntregado } }.toSet()
 
-    fun obtenerProductosPedidos(): Set<Producto> {
-        val setProducto = mutableSetOf<Producto>()
-        clientes.forEach { it -> it.obtenerProductosPedidos().forEach{setProducto.add(it)} }
-        return setProducto.toSet()
-    }
+    fun obtenerProductosPedidos(): Set<Producto> = clientes.flatMap { it.pedidos }.flatMap { it.productos }.toSet()
 
-    fun obtenerProductosPedidosPorTodos():Set<Producto>{
+    fun obtenerProductosPedidosPorTodos(): Set<Producto> {
         val setProducto = mutableSetOf<Producto>()
-        clientes.forEach{ it.pedidos.forEach {it.productos.all }
-    }
-    }
+        clientes.forEach { it.pedidos.forEach { it.productos.all } }
+    }//todo
 
-    fun obtenerNumeroVecesProductoPedido(producto:Producto):Int{
-        clientes.forEach
+    fun obtenerNumeroVecesProductoPedido(producto: Producto): Int {
+        var counter = 0
+        clientes.forEach { cliente -> cliente.pedidos.forEach { pedido -> counter += pedido.productos.count { producto1 -> producto1 == producto } } }
+        return counter
     } //TODO
 
-    fun agrupaClientesPorCiudad():Map<Ciudad,List<Clientes>>{} //TODO
+    fun agrupaClientesPorCiudad(): Map<Ciudad, List<Clientes>> {
+        val mapClientesCiudad = mutableMapOf<Ciudad, List<Clientes>>()
+        val setCiudades = mutableSetOf<Ciudad>()
+        clientes.forEach { setCiudades.add(it.ciudad) }
+        setCiudades.forEach { mapClientesCiudad[it] = obtenerClientesPor(it) }
+        return mapClientesCiudad.toMap()
+    }
 
-    fun mapeaNombreACliente(): Map<String,Clientes>{} //TODO
+    fun mapeaNombreACliente(): Map<String, Clientes> {
+        val mapClientes = mutableMapOf<String, Clientes>()
+        clientes.forEach { mapClientes[it.nombre] = it }
+        return mapClientes.toMap()
+    }
 
-    fun mapeaClienteACiudad(): Map<Clientes,Ciudad>{} //TODO
+    fun mapeaClienteACiudad(): Map<Clientes, Ciudad> {
+        val mapClienteCiudad = mutableMapOf<Clientes, Ciudad>()
+        clientes.forEach { mapClienteCiudad[it] = it.ciudad }
+        return mapClienteCiudad.toMap()
+    }
 
-    fun mapeaNombreClienteACiudad
+    fun mapeaNombreClienteACiudad(): Map<String,Ciudad>{
+        val mapNombreClientesCiudad = mutableMapOf<String,Ciudad>()
+        clientes.forEach { mapNombreClientesCiudad[it.nombre] = it.ciudad }
+        return mapNombreClientesCiudad
+    }
+
+    fun obtenerClienteConMaxPedidos(): Clientes?{
+        val listaClientesNumeroPedidos = mutableListOf<Pair<Clientes,Int>>()
+        clientes.forEach { listaClientesNumeroPedidos.add(Pair(it,it.pedidos.size)) }
+    }//TODO
+
 
 
 }
@@ -57,20 +76,26 @@ data class Tienda(val nombre: String, val clientes: List<Clientes>) {
 data class Clientes(val nombre: String, val ciudad: Ciudad, val pedidos: List<Pedido>) {
     override fun toString() = "$nombre from ${ciudad.nombre}"
 
-    fun obtenerProductosPedidos(): List<Producto> {
-        val listaProductos = mutableListOf<Producto>()
-        pedidos.forEach { it -> it.productos.forEach { listaProductos.add(it) } }
-        return listaProductos.toList()
+    fun obtenerProductoMasCaroPedido():Producto?{
+        val listaProductoPrecio = mutableListOf<Pair<Producto, Double>>()
+        pedidos.forEach { pedido -> pedido.productos.forEach { listaProductoPrecio.add(Pair(it, it.precio)) } }
+        return listaProductoPrecio.find { par -> par.second == listaProductoPrecio.maxOf { it.second } }?.first
     }
 
-    fun encuentraProductoMasCaro():Producto?{
-        val list = mutableListOf<Double>()
-        pedidos.filter{it.estaEntregado}.forEach { it.productos.forEach { list.add(maxOf(it.precio))}}
-        val precio = list.maxByOrNull { it }
-        val lista = mutableListOf<Producto?>()
-        pedidos.forEach { it -> lista.add(it.productos.find { it.precio == precio }) }
-        return lista.maxByOrNull { it?.precio ?: 0.0 }
-    } //TODO
+    fun dineroGastado(): Double {
+        var dineroGastado = 0.0
+        pedidos.forEach { it -> it.productos.forEach { dineroGastado += it.precio } }
+        return dineroGastado
+    }
+
+    fun obtenerProductosPedidos(): List<Producto> = pedidos.flatMap { it.productos }
+
+    fun encuentraProductoMasCaro(): Producto? {
+        val listaProductoPrecio = mutableListOf<Pair<Producto, Double>>()
+        pedidos.filter { it.estaEntregado }
+            .forEach { pedido -> pedido.productos.forEach { listaProductoPrecio.add(Pair(it, it.precio)) } }
+        return listaProductoPrecio.find { par -> par.second == listaProductoPrecio.maxOf { it.second } }?.first
+    }
 }
 
 data class Pedido(val productos: List<Producto>, val estaEntregado: Boolean)
